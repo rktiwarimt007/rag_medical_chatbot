638d6087a423a2785a5ffde7956a630ab0a6802d
Rahul Kumar Tiwari <r.k.tiwari.mt007@gmail.com>
2026-02-14 01:51:07 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating directorties for the project
+mkdir -p src
+mkdir -p research
+
+# Crearing files for the project
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Project structure created successfully!"
